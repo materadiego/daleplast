4df8c6d6203eb889ed09c945f6ab4dbd2b974dcd
vitto tezza <vittorio.tezza93@gmail.com>
2022-10-12 20:24:58 -0300

    footer sin terminar 2

--- daleplast/src/App.js
+++ daleplast/src/App.js
@@ -1,6 +1,6 @@
-import Contact from "./Components/Contact/Contact";
 import NavBar from "./Components/NavBar/NavBar";
 import Homepage from "./Pages/HomePage";
+import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import "./scss/Style.scss";
 
@@ -9,11 +9,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Homepage />
-<<<<<<< HEAD
-      <Footer />
-=======
       <Contact />
->>>>>>> 2620126eae60256d7b24436269a7303b7ef8946d
+      <Footer />
     </div>
   );
 }

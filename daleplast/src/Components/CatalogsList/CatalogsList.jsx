a2acc2edd82139ad918d35fac3d54191facca07a
vitto tezza <vittorio.tezza93@gmail.com>
2022-11-11 10:20:54 -0300

    catalog semi terminado

--- daleplast/src/Components/CatalogsList/CatalogsList.jsx
+++ daleplast/src/Components/CatalogsList/CatalogsList.jsx
@@ -4,14 +4,6 @@ import CatalogsItem from "../CatalogsItem/CatalogsItem";
 function CatalogsList({ products }) {
   return (
     <div className="CatalogCardsContainer">
-      <div className="CategoryTitleContainer">
-        <div className="Orange-Line"></div>
-        <p className="CategoryTitleContainer__Title">Category Title</p>
-        <p className="CategoryTitleContainer__Description">
-          Category Description
-        </p>
-      </div>
-
       {products.map((product) => (
         <CatalogsItem key={product.id} product={product} />
       ))}

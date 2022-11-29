64b99d7f88cadedf44c627d91b2176281673dea6
vitto tezza <vittorio.tezza93@gmail.com>
2022-11-29 10:08:44 -0300

    catalog terminado

--- daleplast/src/Pages/CatalogsPage.jsx
+++ daleplast/src/Pages/CatalogsPage.jsx
@@ -3,12 +3,18 @@ import CatalogsTitle from "../Components/CatalogsTitle/CatalogsTitle";
 import CatalogsMenu from "../Components/CatalogsMenu/CatalogsMenu";
 import CatalogsItemContainer from "../Components/CatalogsItemContainer/CatalogsItemContainer";
 import "../scss/CatalogsStyls.scss";
+
 const CatalogsPage = () => {
   return (
     <div className="Catalogs-Page">
       <CatalogsTitle />
       <CatalogsMenu />
       <CatalogsItemContainer />
+      <div className="UpContainerCatalog">
+        <a className="UpContainerCatalog__Link" href="#Top">
+          <div className="UpContainerCatalog__Link--Image"></div>
+        </a>
+      </div>
     </div>
   );
 };

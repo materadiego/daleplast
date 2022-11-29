9fe24fdcf3f15cdb8e8b2f52f524e43553b18233
Diego Matera <diegomatera@gmail.com>
2022-11-29 13:07:04 -0300

    pagina nuestro compromiso terminada

--- daleplast/src/Components/CompromiseContent/CompromiseContent.jsx
+++ daleplast/src/Components/CompromiseContent/CompromiseContent.jsx
@@ -7,6 +7,7 @@ import icon1 from "../../utils/images/compromise-icon1.svg";
 import icon2 from "../../utils/images/compromise-icon2.svg";
 import icon3 from "../../utils/images/compromise-icon3.svg";
 import CompromiseContentSlider from "./CompromiseContentSlider";
+import CompromiseContentSlider2 from "./CompromiseContentSlider2";
 
 const CompromiseContent = () => {
   return (
@@ -23,88 +24,106 @@ const CompromiseContent = () => {
       {/* SECTION 1 */}
       <div className="Compromise-Section Compromise-Section1">
         {/* SECTION TITLE */}
-        <div className="Compromise-Section__TitleContainer">
-          <img
-            src={icon1}
-            alt="Icon"
-            className="Compromise-Section__TitleContainer--Image"
-          ></img>
-          <p className="Compromise-Section__TitleContainer--Title">
-            PLANTA DE RECICLADO
+        <div className="Compromise-Section-Container Compromise-Section-Container1">
+          <div className="Compromise-Section-Container__TitleContainer">
+            <img
+              src={icon1}
+              alt="Icon"
+              className="Compromise-Section-Container__TitleContainer--Image"
+            ></img>
+            <p className="Compromise-Section-Container__TitleContainer--Title">
+              PLANTA DE RECICLADO
+            </p>
+          </div>
+          {/* LINE */}
+          <div className="Compromise-Section-Container__GreenLine"></div>
+          <p className="Compromise-Section-Container__DesktopSubtitle">
+            Reciclamos el 100% de nuestro residuo post-industrial.
           </p>
-        </div>
-        {/* LINE */}
-        <div className="Compromise-Section__GreenLine"></div>
-        {/* SECTION CONTENT */}
-        <div className="Compromise-Section__Content">
-          <img
-            className="Compromise-Section__Content--Image"
-            src={check}
-            alt="Icon"
-          ></img>
-          El reciclado es un proceso físico-mecánico por el cual el plástico,
-          luego de ser utilizado, es procesado nuevamente para recuperar su
-          valor de uso y poderreinicar el proceso de producción.
-        </div>
-        <div className="Compromise-Section__Content">
-          <img
-            className="Compromise-Section__Content--Image"
-            src={check}
-            alt="Icon"
-          ></img>
-          Se realiza para permitir la re-utilización de los materiales que
-          provienen de los Residuos Sólidos Urbanos y/o, en este caso, del
-          residuo industrial.
+
+          {/* SECTION CONTENT */}
+          <div className="Compromise-Section-Container__Content">
+            <img
+              className="Compromise-Section-Container__Content--Image"
+              src={check}
+              alt="Icon"
+            ></img>
+            El reciclado es un proceso físico-mecánico por el cual el plástico,
+            luego de ser utilizado, es procesado nuevamente para recuperar su
+            valor de uso y poderreinicar el proceso de producción.
+          </div>
+          <div className="Compromise-Section-Container__Content">
+            <img
+              className="Compromise-Section-Container__Content--Image"
+              src={check}
+              alt="Icon"
+            ></img>
+            Se realiza para permitir la re-utilización de los materiales que
+            provienen de los Residuos Sólidos Urbanos y/o, en este caso, del
+            residuo industrial.
+          </div>
         </div>
         <img
           src={plantaReciclado}
           alt=""
-          className="Compromise-Section__Image"
+          className="Compromise-Section__Image Compromise-Image1"
         ></img>
       </div>
       {/* SECTION 2 */}
       <div className="Compromise-Section Compromise-Section2">
         {/* SECTION TITLE */}
-        <div className="Compromise-Section__TitleContainer">
-          <img
-            src={icon2}
-            alt="Icon"
-            className="Compromise-Section__TitleContainer--Image"
-          ></img>
-          <p className="Compromise-Section__TitleContainer--Title Compromise-SectionTitle2">
-            ETAPAS DEL RECICLADO
-          </p>
-        </div>
-        {/* LINE */}
-        <div className="Compromise-Section__GreenLine"></div>
-        {/* SECTION CONTENT */}
-        <div className="Compromise-Section__Content">
-          <span className="Compromise-Section__Content--Span">1.</span>{" "}
-          Recolección, separación y clasificación del scrap según tipo de
-          material y color.
-        </div>
-        <div className="Compromise-Section__Content">
-          <span className="Compromise-Section__Content--Span">2.</span>
-          Tratamiento mecánico del scrap para obtener el material agrumado.
-        </div>
-        <div className="Compromise-Section__Content">
-          <span className="Compromise-Section__Content--Span">3.</span> El
-          agrumado se convierte en pellet tras exponerse a un proceso de alta
-          temperatura, agua, secado y ventilación.
-        </div>
-        <div className="Compromise-Section__Content">
-          <span className="Compromise-Section__Content--Span">4.</span>
-          El material en forma de pelet está listo para reiniciarn su proceso de
-          fabricación.
+        <div className="Compromise-Section-Container Compromise-Section-Container2">
+          <div className="Compromise-Section-Container__TitleContainer">
+            <img
+              src={icon2}
+              alt="Icon"
+              className="Compromise-Section-Container__TitleContainer--Image"
+            ></img>
+            <p className="Compromise-Section-Container__TitleContainer--Title Compromise-SectionTitle2">
+              ETAPAS DEL RECICLADO
+            </p>
+          </div>
+          {/* LINE */}
+
+          <div className="Compromise-Section-Container__GreenLine"></div>
+          {/* SECTION CONTENT */}
+
+          <div className="Compromise-Section-Container__Content">
+            <span className="Compromise-Section-Container__Content--Span">
+              1.
+            </span>{" "}
+            Recolección, separación y clasificación del scrap según tipo de
+            material y color.
+          </div>
+          <div className="Compromise-Section-Container__Content">
+            <span className="Compromise-Section-Container__Content--Span">
+              2.
+            </span>
+            Tratamiento mecánico del scrap para obtener el material agrumado.
+          </div>
+          <div className="Compromise-Section-Container__Content">
+            <span className="Compromise-Section-Container__Content--Span">
+              3.
+            </span>{" "}
+            El agrumado se convierte en pellet tras exponerse a un proceso de
+            alta temperatura, agua, secado y ventilación.
+          </div>
+          <div className="Compromise-Section-Container__Content">
+            <span className="Compromise-Section-Container__Content--Span">
+              4.
+            </span>
+            El material en forma de pelet está listo para reiniciarn su proceso
+            de fabricación.
+          </div>
         </div>
         <img
           src={plantaEtapa}
           alt=""
-          className="Compromise-Section__Image"
+          className="Compromise-Section__Image Compromise-Image2"
         ></img>
       </div>
       {/* SECTION 3 */}
-      <div className="Compromise-Section Compromise-Section3">
+      <div className="Compromise-Section Compromise-Section3 Compromise-Slider-Mobile">
         {/* SECTION TITLE */}
         <div className="Compromise-Section__TitleContainer">
           <img
@@ -126,6 +145,7 @@ const CompromiseContent = () => {
           className="Compromise-Section__Image"
         ></img>
       </div>
+      <CompromiseContentSlider2 />
     </div>
   );
 };
---
+++ daleplast/src/Components/CompromiseContent/CompromiseContentSlider2.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import { useState } from "react";
+import sliderIcon1 from "../../utils/images/compromise-slider-icon1.svg";
+import sliderIcon2 from "../../utils/images/compromise-slider-icon2.svg";
+import icon3 from "../../utils/images/compromise-icon3.svg";
+import buttonImage from "../../utils/images/compromise-slider2button.svg";
+import plantaFabricante from "../../utils/images/compromise-fabricantes.png";
+
+const CompromiseContentSlider2 = () => {
+  const [sliderActive, setSliderActive] = useState(false);
+  const toggleSlider = () => {
+    setSliderActive(!sliderActive);
+    console.log("toggle");
+    console.log(sliderActive);
+  };
+  const togglePagination1 = () => {
+    setSliderActive(false);
+  };
+  const togglePagination2 = () => {
+    setSliderActive(true);
+  };
+
+  return (
+    <div className="Compromise-Slider-Desktop">
+      <div
+        className={`DesktopSlider-Container ${
+          sliderActive ? "Desktop-Slide-Active" : ""
+        }`}
+      >
+        {/* SLIDE 1 */}
+        <div className="Compromise-Desktop-Slide1">
+          <div className="Compromise-Desktop-Slide1__TitleContainer">
+            <img
+              src={icon3}
+              alt="Icon"
+              className="Compromise-Desktop-Slide1__TitleContainer--Image"
+            ></img>
+            <p className="Compromise-Desktop-Slide1__TitleContainer--Title">
+              SOMOS FABRICANTES CERTIFICADOS DE D2W
+            </p>
+          </div>
+          {/* LINE */}
+          <div className="Compromise-Desktop-Slide1__GreenLine"></div>
+          {/* SECTION CONTENT */}
+          <p className="Compromise-Desktop-Slide1__Content">
+            La tecnología D2W le brinda al plástico una vida útil de entre 2 y 5
+            años, esto significa que cumplido este tiempo se degrada evitando la
+            contaminación del suelo, a diferencia del plástico convencional que
+            demora hasta 200 años en desaparecer del ambiente.
+          </p>
+          <div className="Slider-Button" onClick={toggleSlider}>
+            <p className="Slider-Button__Text">
+              CONOCÉ MÁS SOBRE EL ADITIVO D2W
+            </p>
+            <img
+              className="Slider-Button__Image"
+              src={buttonImage}
+              alt="Button"
+            ></img>
+          </div>
+          <img
+            src={plantaFabricante}
+            alt=""
+            className="Compromise-Desktop-Slide1__Image"
+          ></img>
+        </div>
+        {/* SLIDE 2 */}
+        <div className="Compromise-Desktop-Slide2">
+          <div className="Compromise-Desktop-Slide2__TitleContainer">
+            <img
+              src={icon3}
+              alt="Icon"
+              className="Compromise-Desktop-Slide2__TitleContainer--Image"
+            ></img>
+            <p className="Compromise-Desktop-Slide2__TitleContainer--Title">
+              ¿QUÉ ES EL ADITIVO OXI-BIODEGRADABLE?
+            </p>
+          </div>
+          {/* LINE */}
+          <div className="Compromise-Desktop-Slide2__GreenLine"></div>
+          {/* SECTION CONTENT */}
+          <p className="Compromise-Desktop-Slide2__Content">
+            Se trata de un componente que se añade en forma de pellet durante el
+            proceso de fabricación, para alterar la composición del polímero de
+            polietileno tradicional y convertirlo en degradable. Ha sido
+            aprobado en pruebas de eco toxicidad y es seguro al contacto directo
+            con alimentos.
+          </p>
+          <div className="Slider-Button2" onClick={toggleSlider}>
+            <img
+              className="Slider-Button2__Image"
+              src={buttonImage}
+              alt="Button"
+            ></img>
+          </div>
+          <div className="Compromise-Desktop-Slide2__Subcontainer">
+            <p className="Compromise-Desktop-Slide2__Subcontainer--Title">
+              La oxi-biodegradabilidad ocurre en dos períodos:
+            </p>
+            <div className="Compromise-Desktop-Slide2-Box">
+              <img
+                src={sliderIcon1}
+                alt="Fragmentos"
+                className="Compromise-Desktop-Slide2-Box__Image"
+              ></img>
+              <p className="Compromise-Desktop-Slide2-Box__Title">
+                1° Fragmentos moleculares
+              </p>
+              <p className="Compromise-Desktop-Slide2-Box__Description">
+                El plástico se convierte, bajo condiciones específicas de calor,
+                humedad, luz y oxígeno, en fragmentos moleculares menores.
+              </p>
+            </div>
+            <div className="Compromise-Desktop-Slide2-Box">
+              <img
+                src={sliderIcon2}
+                alt="Fragmentos"
+                className="Compromise-Desktop-Slide2-Box__Image"
+              ></img>
+              <p className="Compromise-Desktop-Slide2-Box__Title">
+                2° Biodegradación
+              </p>
+              <p className="Compromise-Desktop-Slide2-Box__Description">
+                Estos fragmentos se biodegradan, lo cual significa que son
+                convertidos en dióxido de carbono, agua y biomasa gracias a
+                microorganismos corruptores.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="PaginationContainer">
+        <div
+          className={`PaginationContainer__Checkbox ${
+            sliderActive ? "" : "Desktop-Slide-Active-Check"
+          }`}
+          onClick={togglePagination1}
+        ></div>
+        <div
+          className={`PaginationContainer__Checkbox ${
+            sliderActive ? "Desktop-Slide-Active-Check" : ""
+          }`}
+          onClick={togglePagination2}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default CompromiseContentSlider2;
